fbc81bf4b541e56b6cd4350b2f8c30fe732862c1
Bhavanashahare <bhavanashahare05@gmail.com>
2023-10-12 09:13:07 +0530

    scantum package

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Auth;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+use Validator;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function loginUser(Request $request): Response
+    {
+        $validator = FacadesValidator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+
+            return Response(['message' => $validator->errors()],401);
+        }
+
+        // if(Auth::attempt($request->all()))
+                // if(Auth::attempt(['email' => $request->email,'password'=>$request->password]))
+
+
+        // if(Auth::attempt('email' => $request->email,'password'=>$request->password))
+
+        {
+
+            // $user = Auth::user();
+            $code = User::where('email', $request->email)->where('password',$request->password)->first();
+
+            $success =  $code->createToken('MyApp')->plainTextToken;
+
+            return Response(['token' => $success],200);
+        }
+
+        return Response(['message' => 'email or password wrong'],401);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function userDetails(): Response
+    {
+        // if (Auth::check()) {
+
+        //     $user = Auth::user();
+
+        $code = User::where(['email', $request->email])->where('password',$request->password)->first();
+
+            return Response(['data' => $user],200);
+
+
+        return Response(['data' => 'Unauthorized'],401);
+}
+
+    /**
+     * Display the specified resource.
+     */
+    public function logout(): Response
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return Response(['data' => 'User Logout successfully.'],200);
+    }
+}
